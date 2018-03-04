9f6f534cfa1c95a982962cbd0dbf1f1cca4a0104
mkushelman <mar07zar@gmail.com>
2018-03-04 09:54:58 -0500

    Add files via upload

---
+++ Project/GetData.r
@@ -0,0 +1,13 @@
+> 
+pathWD <- getwd()
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+f <- "Dataset.zip"
+download.file(url, file.path(pathWD, f))
+exeFILE <- file.path("C:", "Program Files", "7-Zip", "7z.exe")
+zipFile <- file.path(pathWD, f)
+a <- paste0("\"", exeFILE, "\"")
+b <- paste0("\"", zipFile, "\"")
+parameter <- "x"
+cmd <- paste(a, parameter, b)
+system(cmd)
+
